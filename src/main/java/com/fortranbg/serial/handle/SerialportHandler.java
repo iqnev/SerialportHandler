1f03c7226f2d9c0fad5b72b01085d8bd67945cd1
fortran <qnev89@gmail.com>
2017-02-10 22:56:47 +0200

    change fromater to use spaces instead of tabs?

--- src/main/java/com/fortranbg/serial/handle/SerialportHandler.java
+++ src/main/java/com/fortranbg/serial/handle/SerialportHandler.java
@@ -30,11 +30,11 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.TooManyListenersException;
 
-
 /**
- * The <code>SerialportHandler</code> class representation <b>Serial Communication</b>. Serial
- * communication is used for all long-haul communication and most computer networks, where the cost
- * of cable and synchronization difficulties make parallel communication impractical.
+ * The <code>SerialportHandler</code> class representation <b>Serial
+ * Communication</b>. Serial communication is used for all long-haul
+ * communication and most computer networks, where the cost of cable and
+ * synchronization difficulties make parallel communication impractical.
  * 
  * @author Ivelin Yanev <bgfortran@gmail.com>
  * @since 2017
@@ -42,249 +42,256 @@ import java.util.TooManyListenersException;
  */
 public class SerialportHandler {
 
-  private SerialPort serialPort;
-  private OutputStream outStream;
-  private InputStream inStream;
+    private SerialPort serialPort;
+    private OutputStream outStream;
+    private InputStream inStream;
 
-  /**
-   * The filed with a DataRate value.
-   */
-  private int dataRate;
+    /**
+     * The filed with a DataRate value.
+     */
+    private int dataRate;
 
-  /**
-   * The value of TIME OUT constant.
-   */
-  public static final int DEFAULT_TIME_OUT = 2000;
+    /**
+     * The value of TIME OUT constant.
+     */
+    public static final int DEFAULT_TIME_OUT = 2000;
 
-  /**
-   * The filed with a TimeOut value.
-   */
-  private int timeOut;
+    /**
+     * The filed with a TimeOut value.
+     */
+    private int timeOut;
 
-
-  /**
-   * Static member class member that holds only one instance of the <code>StaticHolder</code> class.
-   *
-   */
-  private static class StaticHolder {
-    static final SerialportHandler INSTANCE = new SerialportHandler();
-  }
-
-  /**
-   * Providing Global point of access.
-   * 
-   * @return {@link StaticHolder} object.
-   */
-  public static SerialportHandler getSingleton() {
-    return StaticHolder.INSTANCE;
-  }
-
-  /**
-   * A private constructor, which prevents any other class from instantiating.
-   */
-  private SerialportHandler() {}
-
-  /**
-   * List the available serial ports.
-   * 
-   * @return the {@link ArrayList} with all available serial port names.
-   */
-  public  ArrayList<String> listSerialPorts() {
-    Enumeration<?> portEnum = CommPortIdentifier.getPortIdentifiers();
-
-    ArrayList<String> ports = new ArrayList<String>();
-    while (portEnum.hasMoreElements()) {
-      CommPortIdentifier port = (CommPortIdentifier) portEnum.nextElement();
-      if (port.getPortType() == CommPortIdentifier.PORT_SERIAL) {
-        ports.add(port.getName());
-      }
-    }
-    return ports;
-  }
-
-  /**
-   * Opens a CommPort using {@link CommPortIdentifier} object. If the port is owned by some other
-   * application the method throws {@link IOException}.
-   * 
-   * @param portName the {@link CommPortIdentifier} object.
-   * @throws IOException if {@link IOException} occurs.
-   */
-  public void openPort(final String portName) throws IOException {
-    try {
-      if (portName == null) {
-        throw new NullPointerException("Com port is null");
-      }
-      // Obtain a CommPortIdentifier object for the port you want to open
-      CommPortIdentifier portId;
-
-      portId = CommPortIdentifier.getPortIdentifier(portName);
-
-      if (portId.isCurrentlyOwned()) {
-        throw new IllegalStateException("Com port in use");
-      }
-
-      // Get the port's ownership
-      this.serialPort = (SerialPort) portId.open(SerialportHandler.class.getName(), this.timeOut);
-
-      // Set the parameters of the connection.
-      setSerialPortParameters();
-
-      this.serialPort.notifyOnDataAvailable(true);
-
-      // Open the input and output streams for the connection.
-      // If they won't open, close the port before throwing an
-      // exception.
-      outStream = serialPort.getOutputStream();
-      inStream = serialPort.getInputStream();
-    } catch (NoSuchPortException e1) {
-      throw new IOException(e1.getMessage());
-    } catch (PortInUseException e) {
-      throw new IOException(e.getMessage());
-    } catch (IOException e) {
-      this.serialPort.close();
-      throw e;
+    /**
+     * Static member class member that holds only one instance of the
+     * <code>StaticHolder</code> class.
+     *
+     */
+    private static class StaticHolder {
+        static final SerialportHandler INSTANCE = new SerialportHandler();
     }
 
-  }
+    /**
+     * Providing Global point of access.
+     * 
+     * @return {@link StaticHolder} object.
+     */
+    public static SerialportHandler getSingleton() {
+        return StaticHolder.INSTANCE;
+    }
 
-  /**
-   * Check for available data.
-   * 
-   * @return
-   */
-  public boolean isDataAvailable() {
-    boolean flag = false;
-    try {
-      if (this.inStream.available() != 0) {
-        flag = true;
-      }
-    } catch (IOException ex) {
+    /**
+     * A private constructor, which prevents any other class from instantiating.
+     */
+    private SerialportHandler() {
+    }
+
+    /**
+     * List the available serial ports.
+     * 
+     * @return the {@link ArrayList} with all available serial port names.
+     */
+    public ArrayList<String> listSerialPorts() {
+        Enumeration<?> portEnum = CommPortIdentifier.getPortIdentifiers();
+
+        ArrayList<String> ports = new ArrayList<String>();
+        while (portEnum.hasMoreElements()) {
+            CommPortIdentifier port = (CommPortIdentifier) portEnum.nextElement();
+            if (port.getPortType() == CommPortIdentifier.PORT_SERIAL) {
+                ports.add(port.getName());
+            }
+        }
+        return ports;
+    }
+
+    /**
+     * Opens a CommPort using {@link CommPortIdentifier} object. If the port is
+     * owned by some other application the method throws {@link IOException}.
+     * 
+     * @param portName
+     *            the {@link CommPortIdentifier} object.
+     * @throws IOException
+     *             if {@link IOException} occurs.
+     */
+    public void openPort(final String portName) throws IOException {
+        try {
+            if (portName == null) {
+                throw new NullPointerException("Com port is null");
+            }
+            // Obtain a CommPortIdentifier object for the port you want to open
+            CommPortIdentifier portId;
+
+            portId = CommPortIdentifier.getPortIdentifier(portName);
+
+            if (portId.isCurrentlyOwned()) {
+                throw new IllegalStateException("Com port in use");
+            }
+
+            // Get the port's ownership
+            this.serialPort = (SerialPort) portId.open(SerialportHandler.class.getName(), this.timeOut);
+
+            // Set the parameters of the connection.
+            setSerialPortParameters();
+
+            this.serialPort.notifyOnDataAvailable(true);
+
+            // Open the input and output streams for the connection.
+            // If they won't open, close the port before throwing an
+            // exception.
+            outStream = serialPort.getOutputStream();
+            inStream = serialPort.getInputStream();
+        } catch (NoSuchPortException e1) {
+            throw new IOException(e1.getMessage());
+        } catch (PortInUseException e) {
+            throw new IOException(e.getMessage());
+        } catch (IOException e) {
+            this.serialPort.close();
+            throw e;
+        }
 
     }
 
-    return flag;
-  }
+    /**
+     * Check for available data.
+     * 
+     * @return
+     */
+    public boolean isDataAvailable() {
+        boolean flag = false;
+        try {
+            if (this.inStream.available() != 0) {
+                flag = true;
+            }
+        } catch (IOException ex) {
 
-  /**
-   * Disconnect the serial port.
-   */
-  public void disconnect() {
-    if (serialPort != null) {
-      try {
-        // close the i/o streams.
-        outStream.close();
-        inStream.close();
-      } catch (IOException ex) {
-        // don't care
-      }
-      // Close the port.
-      serialPort.close();
-      serialPort = null;
+        }
+
+        return flag;
     }
-  }
 
-  /**
-   * Sets TimeOut of serial connection
-   *
-   * @param timeOut the TimeOut value.
-   */
-  public void setTimeOut(final int timeOut) {
-    this.timeOut = timeOut;
-  }
-
-  /**
-   * Sets DataRade of the serial connection.
-   *
-   * @param dataRate the DataRade value.
-   */
-  public void setDataRate(final int dataRate) {
-    this.dataRate = dataRate;
-  }
-
-  /**
-   * @return the outStream
-   */
-  public OutputStream getOutStream() {
-    return outStream;
-  }
-
-  /**
-   * @return the inStream
-   */
-  public InputStream getInStream() {
-    return inStream;
-  }
-
-  /**
-   * Sets the serial port parameters.
-   * 
-   * @throws IOException
-   * 
-   * @throws UnsupportedCommOperationException
-   */
-  protected void setSerialPortParameters() throws IOException {
-    try {
-      this.serialPort.setSerialPortParams(this.dataRate, SerialPort.DATABITS_8,
-          SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
-    } catch (UnsupportedCommOperationException e) {
-      throw new IOException("Unsupported serial port parameter");
+    /**
+     * Disconnect the serial port.
+     */
+    public void disconnect() {
+        if (serialPort != null) {
+            try {
+                // close the i/o streams.
+                outStream.close();
+                inStream.close();
+            } catch (IOException ex) {
+                // don't care
+            }
+            // Close the port.
+            serialPort.close();
+            serialPort = null;
+        }
     }
-  }
 
-  /**
-   * Adds the specified {@link SerialPortEventListener} listener to receive
-   * {@link SerialportHandler} events. If listener deviceListener is null, no exception is thrown
-   * and no action is performed.
-   * 
-   * @param listener the {@link SerialPortEventListener} object.
-   * @throws TooManyListenersException if {@link TooManyListenersException} occurs.
-   */
-  public void addSerialEventListener(final SerialPortEventListener listener)
-      throws TooManyListenersException {
-    this.serialPort.addEventListener(listener);
-  }
-
-  /**
-   * This method reads block of data with size.
-   * 
-   * @param len the number of bytes to read.
-   * @return the bytes of array.
-   * @throws IOException if {@link IOException} occurs.
-   */
-  public byte[] readBlocked(final int len) throws IOException {
-    byte[] buff = new byte[len];
-
-    DataInputStream dInputStream = new DataInputStream(this.inStream);
-    dInputStream.readFully(buff, 0, len);
-
-    return buff;
-  }
-
-  /*
-   * Writes the specified byte to this byte array output stream.
-   * 
-   * @param bytes the byte to be written.
-   * 
-   * @throws IOException if {@link IOException} occurs.
-   */
-  public void writeBlock(final byte[] bytes) throws IOException {
-    if (bytes == null) {
-      throw new NullPointerException("Byte buffer is empty");
+    /**
+     * Sets TimeOut of serial connection
+     *
+     * @param timeOut
+     *            the TimeOut value.
+     */
+    public void setTimeOut(final int timeOut) {
+        this.timeOut = timeOut;
     }
-    this.outStream.write(bytes);
-  }
 
-  /**
-   * Gets all available data from input stream.
-   * 
-   * @return the available data.
-   * @throws IOException if {@link IOException} occurs.
-   */
-  public int getAvailableBytes() throws IOException {
-    return this.inStream.available();
+    /**
+     * Sets DataRade of the serial connection.
+     *
+     * @param dataRate
+     *            the DataRade value.
+     */
+    public void setDataRate(final int dataRate) {
+        this.dataRate = dataRate;
+    }
 
-  }
+    /**
+     * @return the outStream
+     */
+    public OutputStream getOutStream() {
+        return outStream;
+    }
 
+    /**
+     * @return the inStream
+     */
+    public InputStream getInStream() {
+        return inStream;
+    }
 
+    /**
+     * Sets the serial port parameters.
+     * 
+     * @throws IOException
+     * 
+     * @throws UnsupportedCommOperationException
+     */
+    protected void setSerialPortParameters() throws IOException {
+        try {
+            this.serialPort.setSerialPortParams(this.dataRate, SerialPort.DATABITS_8, SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE);
+        } catch (UnsupportedCommOperationException e) {
+            throw new IOException("Unsupported serial port parameter");
+        }
+    }
+
+    /**
+     * Adds the specified {@link SerialPortEventListener} listener to receive
+     * {@link SerialportHandler} events. If listener deviceListener is null, no
+     * exception is thrown and no action is performed.
+     * 
+     * @param listener
+     *            the {@link SerialPortEventListener} object.
+     * @throws TooManyListenersException
+     *             if {@link TooManyListenersException} occurs.
+     */
+    public void addSerialEventListener(final SerialPortEventListener listener) throws TooManyListenersException {
+        this.serialPort.addEventListener(listener);
+    }
+
+    /**
+     * This method reads block of data with size.
+     * 
+     * @param len
+     *            the number of bytes to read.
+     * @return the bytes of array.
+     * @throws IOException
+     *             if {@link IOException} occurs.
+     */
+    public byte[] readBlocked(final int len) throws IOException {
+        byte[] buff = new byte[len];
+
+        DataInputStream dInputStream = new DataInputStream(this.inStream);
+        dInputStream.readFully(buff, 0, len);
+
+        return buff;
+    }
+
+    /*
+     * Writes the specified byte to this byte array output stream.
+     * 
+     * @param bytes the byte to be written.
+     * 
+     * @throws IOException if {@link IOException} occurs.
+     */
+    public void writeBlock(final byte[] bytes) throws IOException {
+        if (bytes == null) {
+            throw new NullPointerException("Byte buffer is empty");
+        }
+        this.outStream.write(bytes);
+    }
+
+    /**
+     * Gets all available data from input stream.
+     * 
+     * @return the available data.
+     * @throws IOException
+     *             if {@link IOException} occurs.
+     */
+    public int getAvailableBytes() throws IOException {
+        return this.inStream.available();
+
+    }
 
 }
